0037074f69728839d70ef1c7ad7971be1764bb6e
misraag <jaiofficial50@gmail.com>
2024-06-17 20:21:31 +0530

    removed props from Product component

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product}/>
           </li>
         ))}
       </Shop>
